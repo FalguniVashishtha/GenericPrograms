372bd04168bffcfae2dab1daaf7208285ae64324
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-09 23:48:53 +0530

    Refractor2

--- src/com/bridgelabz/generic/MaxOfValue.java
+++ src/com/bridgelabz/generic/MaxOfValue.java
@@ -1,22 +1,35 @@
 package com.bridgelabz.generic;
 import java.util.Scanner;
 
-public class MaxOfValue {
-    //Generic Type extends Comparable
-    public static <E extends Comparable<E>> E findMaxValue(E a1, E a2, E a3)
+    public class MaxOfValue <E extends Comparable<E>>
     {
-        E max = a1; //initially a1 is max
-        if (a2.compareTo(max) > 0)
-            max = a2;
-        if (a3.compareTo(max) > 0)
-            max = a3;
-        return max;
+        E a1, a2, a3; //Generics class variables
+
+        //constructor
+        public MaxOfValue(E a1, E a2, E a3)
+        {
+            this.a1 = a1;
+            this.a2 = a2;
+            this.a3 = a3;
+        }
+
+        //Generics method to find max value
+        public static <E extends Comparable<E>> E findMaxValue(E a1, E a2, E a3)
+        {
+            E max = a1; //initially a1 is largest
+            if (a2.compareTo(max) > 0)
+                max = a2;
+            if (a3.compareTo(max) > 0)
+                max = a3;
+            return max;
+        }
+
+        //main method starts
+        public static void main(String[] args)
+        {
+            //printing the max values
+            System.out.println("The maximum of three integers: " + findMaxValue(110, 200, 150));
+            System.out.println("The maximum of three floats: " + findMaxValue(12.33f, 108.7f, 4.11f));
+            System.out.println("The maximum of three strings: " + findMaxValue("zhcb", "qtwyte", "osfb"));
+        }
     }
-    //main method starts
-    public static void main(String[] args) {
-        //printing the max values
-        System.out.println("The maximum of three integers: " +findMaxValue(10,20,30));
-        System.out.println("The maximum of three floats: " +findMaxValue(41.88f, 108.170f, 9.06f));
-        System.out.println("The maximum of three strings: " +findMaxValue("jdsfj", "yqwguy", "ascd"));
-    }
-}
