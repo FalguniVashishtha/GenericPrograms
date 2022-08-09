4503c491814ec55c68ebfab59db5af3360edf533
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-09 22:56:22 +0530

    max of float number

--- src/com/bridgelabz/generic/MaxOfValue.java
+++ src/com/bridgelabz/generic/MaxOfValue.java
@@ -13,10 +13,22 @@ public class MaxOfValue {
             max = b;
         if (c.compareTo(max) > 0)
             max = c;
-        System.out.printf("The Maximum of three numbers (%d,%d,%d): ", a, b, c).println(max);
+        System.out.printf("The Maximum of three Integers (%d,%d and %d): ", a, b, c).println(max);
+    }
+    //method to find max of three floats
+    public static void findMaxValue(Float a , Float b , Float c)
+    {
+        Float max = a; //Initially maximum value is Integer a.
+        if (b.compareTo(max) > 0)
+            max = b;
+        if (c.compareTo(max) > 0)
+            max = c;
+        // Print the max number
+        System.out.printf("The Maximum of three Float Numbers (%f, %f and %f): ",a,b,c).println(max);
     }
     //main method starts
     public static void main(String[] args) {
         MaxOfValue.findMaxValue(); //calling the method
+        MaxOfValue.findMaxValue(10f,20f,30f);
     }
 }
